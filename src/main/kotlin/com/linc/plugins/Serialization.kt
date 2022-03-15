53a4c92a136d31cc4f6d616a4da4141593c9ced7
Andrew <andrew.linc.work@gmail.com>
2022-03-15 23:36:52 +0200

    Fixed build error

--- src/main/kotlin/com/linc/plugins/Serialization.kt
+++ src/main/kotlin/com/linc/plugins/Serialization.kt
@@ -1,24 +1,11 @@
 package com.linc.plugins
 
-import io.ktor.serialization.*
 import io.ktor.features.*
 import io.ktor.application.*
 import io.ktor.gson.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         gson()
     }
-    /*install(ContentNegotiation) {
-        json()
-    }
-
-    routing {
-        get("/json/kotlinx-serialization") {
-                call.respond(mapOf("hello" to "world"))
-            }
-    }*/
 }
