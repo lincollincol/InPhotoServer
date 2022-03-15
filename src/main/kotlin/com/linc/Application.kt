1090839446928663d31734b6d0adcfa68a60473c
Andrew <andrew.linc.work@gmail.com>
2022-03-16 00:18:07 +0200

    Added port

--- src/main/kotlin/com/linc/Application.kt
+++ src/main/kotlin/com/linc/Application.kt
@@ -6,7 +6,8 @@ import io.ktor.server.netty.*
 
 fun main() {
 //    embeddedServer(Netty, port = 8885, host = "0.0.0.0") {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+//    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+    embeddedServer(Netty, port = 8885) {
         configureDi()
         configureDatabase()
 
