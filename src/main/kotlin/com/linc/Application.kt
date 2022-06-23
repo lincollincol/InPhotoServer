0fc395a65b97eecea3f0e9cd5629bb67ce4d705d
Andrew <andrew.linc.work@gmail.com>
2022-06-23 15:48:54 +0300

    System data sync [INPH-45]

--- src/main/kotlin/com/linc/Application.kt
+++ src/main/kotlin/com/linc/Application.kt
@@ -17,6 +17,6 @@ fun main() {
         configureMonitoring()
 
         // TODO: 19.04.22 uncomment when release or new version
-//        synchronizeLocalData()
+        synchronizeLocalData()
     }.start(wait = true)
 }
