f56f52030c56086fce242f4f070ecf9e37d75d2d
Andrew <andrew.linc.work@gmail.com>
2022-06-23 15:29:14 +0300

    Stable version heroku deploy [INPH-45]

--- src/main/kotlin/com/linc/utils/Constants.kt
+++ src/main/kotlin/com/linc/utils/Constants.kt
@@ -2,10 +2,13 @@ package com.linc.utils
 
 object Constants {
 
+    const val JDBC_URL =
+        "jdbc:postgresql://ec2-35-171-171-27.compute-1.amazonaws.com:5432/dff1v40rbobbrn?password=774f00bbd13d916d8d64cabefe9ebb9ef5e8e2330dfef2c046f52f571befd198&sslmode=require&user=yswkdkfclyvpyk"
     //jdbc:postgresql://ec2-35-171-171-27.compute-1.amazonaws.com:5432/dff1v40rbobbrn
 //    const val JDBC_URL = "jdbc:postgresql://ec2-35-171-171-27.compute-1.amazonaws.com:5432/dff1v40rbobbrn?password=774f00bbd13d916d8d64cabefe9ebb9ef5e8e2330dfef2c046f52f571befd198&sslmode=require&user=yswkdkfclyvpyk"
-//    const val JDBC_URL = "jdbc:postgresql://localhost:5432/inphoto_db"
-    const val JDBC_URL = "jdbc:postgresql://localhost:5432/inphoto_db?user=inphoto_admin&password=12345678"
+
+    // localhost
+//    const val JDBC_URL = "jdbc:postgresql://localhost:5432/inphoto_db?user=inphoto_admin&password=12345678"
 
     const val CLOUDINARY_URL = "cloudinary://942731591311994:6_ai7gKJmk0Jr543WHMvNeJ1ubY@dqildid2v"
 
